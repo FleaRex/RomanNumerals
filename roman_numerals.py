cdee1d8250e0d677e325d2a45dd4f009985eb9f7
Ben Madley <bmad29@hotmail.co.uk>
2019-03-03 19:45:50 +0000

    Removing non-roman numerals.

--- roman_numerals.py
+++ roman_numerals.py
@@ -14,7 +14,16 @@ def lambda_handler(event, context):
 
 
 def convert_roman_to_arabic(roman_value):
-    if not isinstance(roman_value, six.string_types):
+    if not is_valid_roman_numerals(roman_value):
         raise Exception("Error: " + str(roman_value) + " is not in Roman Numerals")
-
     return 13
+
+
+def is_valid_roman_numerals(roman_value):
+    if not isinstance(roman_value, six.string_types):
+        return False
+    if not all(
+        map(lambda x: x in ["i", "v", "x", "l", "c", "d", "m"], list(roman_value))
+    ):
+        return False
+    return True
--- tests/test_roman_to_arabic.py
+++ tests/test_roman_to_arabic.py
@@ -4,7 +4,7 @@ from roman_numerals import convert_roman_to_arabic
 
 class TestRomanToArabic(object):
     def test_basic(self):
-        assert convert_roman_to_arabic("test") == 13
+        assert convert_roman_to_arabic("xiii") == 13
 
     def test_non_string_throws(self):
         with pytest.raises(Exception):
@@ -15,3 +15,7 @@ class TestRomanToArabic(object):
             convert_roman_to_arabic(12)
         except Exception as e:
             assert "Error:" in str(e)
+
+    def test_non_roman_throws(self):
+        with pytest.raises(Exception):
+            convert_roman_to_arabic("cxvq")
